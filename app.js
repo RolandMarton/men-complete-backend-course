6d6756b3198b9437bd3b34704f4813bbfa5d5af0
RolandMarton <roland111187@gmail.com>
2025-11-17 19:36:24 +0100

    Add arcjet

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleWare from './middlewares/error.middleware.js';
+import arcjetMiddleWare from './middlewares/arcjet.middleware.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(arcjetMiddleWare);
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
