4eff7d4c85244baaa241b49f5b2e5b9f0505c531
RolandMarton <roland111187@gmail.com>
2025-11-17 17:08:10 +0100

    Add middlewares

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import { PORT } from './config/env.js';
 
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
-
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleWare from './middlewares/error.middleware.js';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
 
+app.use(errorMiddleWare);
+
 app.get('/', (req, res) => {
-  res.send('Welcome to the Subscription Tracker API');
+	res.send('Welcome to the Subscription Tracker API');
 });
 
 app.listen(PORT, async () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+	console.log(`Server is running on port http://localhost:${PORT}`);
 
-  await connectToDatabase();
+	await connectToDatabase();
 });
 
 export default app;
