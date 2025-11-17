df8ce4d07393164181ab1c5749d0fc34b463c545
RolandMarton <roland111187@gmail.com>
2025-11-17 15:34:40 +0100

    Add database

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 
+import connectToDatabase from './database/mongodb.js';
+
 const app = express();
 
 app.use('/api/v1/auth', authRouter);
@@ -15,8 +17,10 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Subscription Tracker API');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
+
+  await connectToDatabase();
 });
 
 export default app;
